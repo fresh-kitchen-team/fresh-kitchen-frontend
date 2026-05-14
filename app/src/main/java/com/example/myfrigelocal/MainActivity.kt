a4d292e69c86eb686c566e8f2d0d004ca3c4c515
donggun <ldg1875@gmail.com>
2026-05-14 20:45:14 +0900

    3213

--- app/src/main/java/com/example/myfrigelocal/MainActivity.kt
+++ app/src/main/java/com/example/myfrigelocal/MainActivity.kt
@@ -12,9 +12,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 임시 테스트용 accessToken 설정
-        // 실제 토큰을 커밋하면 안 됨. 테스트 끝나면 반드시 삭제.
-        AuthTokenStore.setAccessToken("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjIsInJvbGUiOiJVU0VSIiwidG9rZW5UeXBlIjoiYWNjZXNzIiwiaWF0IjoxNzc4NzU4Nzk5LCJleHAiOjE3Nzg3NjA1OTl9.dcWNWGK8PZZrPYHojpLY_zzMBTgW4DyOP9v9B6Gu3f4")
 
         setContent {
             MyFridgeApp()
