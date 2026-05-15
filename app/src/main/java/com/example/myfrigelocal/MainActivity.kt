1d69e1c2e525aea338b415600947978c2ed6dab2
donggun <ldg1875@gmail.com>
2026-05-15 14:08:48 +0900

    api 연동 완료_2

--- app/src/main/java/com/example/myfrigelocal/MainActivity.kt
+++ app/src/main/java/com/example/myfrigelocal/MainActivity.kt
@@ -7,10 +7,11 @@ import androidx.compose.runtime.Composable
 import com.example.myfrigelocal.ui.MainScaffold
 import com.example.myfrigelocal.ui.theme.MyFrigeLocalTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AuthTokenStore.setAccessToken("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjIsInJvbGUiOiJVU0VSIiwidG9rZW5UeXBlIjoiYWNjZXNzIiwiaWF0IjoxNzc4NzcyMjIyLCJleHAiOjE3Nzg3NzQwMjJ9.9xxSg31LvOQQYT0KQiUBvG5Z3HNILGk-XEB1IvxcqNk")
+        AuthTokenStore.setAccessToken("")
         setContent {
             MyFridgeApp()
         }
