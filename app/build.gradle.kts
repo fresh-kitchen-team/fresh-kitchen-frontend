9ac020dc0028af2043e2d0452baf5ea18cb4f534
donggun <ldg1875@gmail.com>
2026-06-02 18:20:04 +0900

    앱 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.freshkitchen.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
