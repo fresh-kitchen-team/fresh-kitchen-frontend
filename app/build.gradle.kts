d4f6058199a59008194ddd963fd6a394b61e5920
donggun <ldg1875@gmail.com>
2026-06-18 21:06:18 +0900

    배포 버전 변경 및 릴리즈

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         applicationId = "com.freshkitchen.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
